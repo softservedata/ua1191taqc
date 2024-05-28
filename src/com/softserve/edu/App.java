bdc85eb8996ff42bef85e4f756f1a31769fa059a
Yaroslav Harasym <garasym@mail.com>
2024-05-28 18:28:16 +0300

    .gitignore Updated

---
+++ src/com/softserve/edu/App.java
@@ -0,0 +1,13 @@
+package com.softserve.edu;
+
+public class App {
+
+    private int i = 123;
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        App m = new App();
+        System.out.printf("i = " + m.i);
+    }
+
+}
