a6e510f10733890dde0779f7eef652d0567bae06
Yaroslav <softservedata@users.noreply.github.com>
2024-05-28 18:50:05 +0300

    Update App.java

--- src/com/softserve/edu/App.java
+++ src/com/softserve/edu/App.java
@@ -5,7 +5,7 @@ public class App {
     private int i = 123;
 
     public static void main(String[] args) {
-        System.out.println("Hello world! from dev branch");
+        System.out.println("Hello world! from dev branch_!!!");
         App m = new App();
         System.out.printf("i = " + m.i);
     }
