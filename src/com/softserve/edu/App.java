7632d6ca3fb27c1159bb086ad9c4c74eb6e7eae4
Yaroslav Harasym <garasym@mail.com>
2024-05-28 18:47:54 +0300

    Message updated

--- src/com/softserve/edu/App.java
+++ src/com/softserve/edu/App.java
@@ -5,7 +5,7 @@ public class App {
     private int i = 123;
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! from dev branch");
         App m = new App();
         System.out.printf("i = " + m.i);
     }
