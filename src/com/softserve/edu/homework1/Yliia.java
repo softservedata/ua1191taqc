81de896d3e762a6239f7bada9b87f1eeb6932330
yuliiapaskal <yuliiapaskal@localhost>
2024-05-29 18:12:06 +0100

    Yuliia class added

---
+++ src/com/softserve/edu/homework1/Yliia.java
@@ -0,0 +1,13 @@
+package com.softserve.edu.homework1;
+
+ 
+
+public class Yliia {
+	  private int i = 123;
+
+	    public static void main(String[] args) {
+	        System.out.println("Hello world! from dev branch_!!!");
+	        Yliia m = new Yliia();
+	        System.out.printf("i = " + m.i);
+	    }
+}
