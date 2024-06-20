622c4767aa4a94b55df6f92ee2c9b9a54fecd339
Yaroslav Harasym <garasym@mail.com>
2024-06-20 20:25:17 +0300

    Selenium WebDriver

--- src/test/java/com/softserve/edu03ide/GreencityTest.java
+++ src/test/java/com/softserve/edu03ide/GreencityTest.java
@@ -6,18 +6,23 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
+import java.util.List;
 
 public class GreencityTest {
+    public static final int MAX_IMPLICITLY_WAIT = 10;
     private static WebDriver driver;
 
     @BeforeAll
     public static void setup() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        //WebDriverManager.firefoxdriver().setup();
+        //driver = new FirefoxDriver();
         //
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(MAX_IMPLICITLY_WAIT));
         driver.manage().window().maximize();
         //
         System.out.println("@BeforeAll executed");
@@ -27,6 +32,7 @@ public class GreencityTest {
     public static void tearDown() {
         if (driver != null) {
             driver.quit();
+            //driver.close();
         }
         //
         System.out.println("@AfterAll executed");
@@ -61,6 +67,21 @@ public class GreencityTest {
         //driver.findElement(By.cssSelector(".main-picture")).click();
         //Thread.sleep(1000);
         //
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+        List<WebElement> iframe = driver.findElements(By.cssSelector("iframe"));
+        System.out.println("\t\t\t\tiframe.size() = " + iframe.size());
+        if (iframe.size() > 0) {
+            driver.switchTo().frame(iframe.get(0));
+            List<WebElement> popupButton = driver.findElements(By.id("close"));
+            System.out.println("\t\t\t\tpopupButton.size() = " + popupButton.size());
+            if (popupButton.size() > 0) {
+                popupButton.get(0).click();
+            }
+            driver.switchTo().defaultContent();
+        }
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+         Thread.sleep(1000);
+        //
         driver.findElement(By.id("email")).click();
         Thread.sleep(1000);
         //
@@ -70,7 +91,15 @@ public class GreencityTest {
         driver.findElement(By.id("password")).click();
         Thread.sleep(1000);
         //
-        driver.findElement(By.id("password")).sendKeys("Qwerty_1"); // Invalid Solution
+        // From OS
+        String javaHome = System.getenv("JAVA_HOME");
+        System.out.println("\t\t\tSystem.getenv(\"JAVA_HOME\") = " + javaHome);
+        //
+        // From Eclipse/Idea
+        String password = System.getenv().get("MY_PASSWORD");
+        System.out.println("\t\t\tpassword = " + password);
+        //driver.findElement(By.id("password")).sendKeys("Qwerty_1"); // Invalid Solution
+        driver.findElement(By.id("password")).sendKeys(password);
         Thread.sleep(1000);
         //
         driver.findElement(By.cssSelector("button.ubsStyle")).click();
@@ -94,4 +123,62 @@ public class GreencityTest {
         //
         Assertions.assertTrue(signup.getText().contains("Sign up"));
     }
+
+    @Test
+    public void checkMenu() throws InterruptedException {
+        driver.findElement(By.cssSelector("app-header:nth-child(1) .nav-left-list:nth-child(4) > .url-name")).click();
+        Thread.sleep(1000); // for presentation only
+        //
+        driver.findElement(By.cssSelector(".nav-left-list:nth-child(6) > .url-name")).click();
+        Thread.sleep(1000);
+        //
+        driver.findElement(By.cssSelector("app-header:nth-child(1) .ubs-header-sing-in-img")).click();
+        Thread.sleep(1000);
+        //
+        driver.findElement(By.id("email")).click();
+        Thread.sleep(1000);
+        //
+        driver.findElement(By.id("email")).sendKeys("tyv09754@zslsz.com");
+        Thread.sleep(1000);
+        //
+        driver.findElement(By.id("password")).click();
+        Thread.sleep(1000);
+        //
+        driver.findElement(By.id("password")).sendKeys("Qwerty_1");
+        Thread.sleep(1000);
+        //
+        driver.findElement(By.cssSelector(".ubsStyle")).click();
+        Thread.sleep(1000);
+        //
+        driver.findElement(By.cssSelector("app-header:nth-child(1) .nav-left-list:nth-child(4) > .url-name")).click();
+        Thread.sleep(1000);
+        //
+        driver.findElement(By.cssSelector(".nav-left-list:nth-child(1) > .url-name")).click();
+        Thread.sleep(1000);
+        //
+        driver.findElement(By.linkText("Events")).click();
+        Thread.sleep(1000);
+        //
+        driver.findElement(By.linkText("Places")).click();
+        Thread.sleep(1000);
+        //
+        driver.findElement(By.cssSelector(".nav-left-list:nth-child(4) > .url-name")).click();
+        Thread.sleep(1000);
+        //
+        driver.findElement(By.cssSelector(".nav-left-list:nth-child(5) > .url-name")).click();
+        Thread.sleep(1000);
+        //
+        driver.findElement(By.id("mat-tab-label-0-0")).click();
+        Thread.sleep(1000);
+        //
+        Assertions.assertEquals("My habits", driver.findElement(By.cssSelector("#mat-tab-label-0-0 .mdc-tab__text-label")).getText());
+        Thread.sleep(1000);
+        //
+        driver.findElement(By.cssSelector(".body-2")).click();
+        Thread.sleep(1000);
+        //
+        driver.findElement(By.linkText("Sign out")).click();
+        Thread.sleep(1000);
+    }
+
 }
---
+++ src/test/java/com/softserve/edu04sel/SeleniumFirstTest.java
@@ -0,0 +1,40 @@
+package com.softserve.edu04sel;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class SeleniumFirstTest {
+
+    private final Long IMPLICITLY_WAIT_SECONDS = 10L;
+
+    @Test
+    public void checkMac() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "./lib/chromedriver.exe");
+        //
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICITLY_WAIT_SECONDS)); // 0 by default
+        driver.manage().window().maximize();
+        //
+        driver.get("https://google.com/");
+        Thread.sleep(2000); // For Presentation
+        //
+        driver.findElement(By.name("q")).sendKeys("mac");
+        Thread.sleep(2000); // For Presentation
+        //
+        //driver.findElement(By.cssSelector("button[type='submit']")).click();
+        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+        //
+        Thread.sleep(8000); // For Presentation
+        //
+        //int i = 1 / 0;
+        //
+        driver.quit();
+    }
+
+
+}
