a7fc6cd6294a3b34c71aed12a50b7d73790c09f2
Nestor <nestor.t721@gmail.com>
2024-06-25 19:27:48 +0300

    task2 done

--- src/com/softserve/edu/introToJava/task2/Task2.java
+++ src/com/softserve/edu/introToJava/task2/Task2.java
@@ -1,5 +1,18 @@
 package com.softserve.edu.introToJava.task2;
 
-public class Task2 {
+import java.util.Scanner;
 
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter temperature for 5 days: ");
+        float temperature = 0.0f;
+        float sum = 0.0f;
+        for(int i = 0; i < 5; i++){
+            temperature = sc.nextFloat();
+            sum += temperature;
+        }
+        float average = sum / 5;
+        System.out.println("Average temperature: " + average);
+    }
 }
