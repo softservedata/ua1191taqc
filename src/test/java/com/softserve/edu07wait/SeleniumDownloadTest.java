2fde7071d18e985dd1f0bd87483903a4b0e76288
Yaroslav Harasym <garasym@mail.com>
2024-06-27 18:53:56 +0300

    Explicit wait. JavaScript injection

--- src/test/java/com/softserve/edu07wait/SeleniumDownloadTest.java
+++ src/test/java/com/softserve/edu07wait/SeleniumDownloadTest.java
@@ -2,12 +2,12 @@ package com.softserve.edu07wait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 
 import java.time.Duration;
 
@@ -123,11 +123,11 @@ public class SeleniumDownloadTest {
         //
         //browserstack.click(); // Error after JS Scrolling
         //
-        // /*
+        /*
         // CLick by JavaScript
         js.executeScript("arguments[0].click();", browserstack);
         presentationSleep(); // For Presentation ONLY
-        /*
+        // /*
         js.executeScript("alert('Welcome to TAQC');");
         presentationSleep(4); // For Presentation ONLY
         //
