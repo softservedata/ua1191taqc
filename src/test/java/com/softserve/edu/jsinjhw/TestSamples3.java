efeed4eec86f2ba8e2e4ea663384406abb4c63e8
Nestor <nestor.t721@gmail.com>
2024-07-19 10:51:26 +0300

    JavaScript Injection. Completed Task

--- src/test/java/com/softserve/edu/jsinjhw/TestSamples3.java
+++ src/test/java/com/softserve/edu/jsinjhw/TestSamples3.java
@@ -24,39 +24,16 @@ import java.util.NoSuchElementException;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 public class TestSamples3 {
-    //@FindBy(id = "email")
-    //private WebElement emailInput;
-    //@FindBy(id = "password")
-    //private WebElement passwordInput;
-    //@FindBy(css = ".ubsStyle")
-    //private WebElement signInSubmitButton;
-    //@FindBy(css = ".mat-simple-snackbar > span")
-    //private WebElement result;
-    //@FindBy(css = ".alert-general-error")
-    //private WebElement errorMessage;
-   // @FindBy(xpath = "//*[@id=\"password-err-msg\"]/app-error/div")
-   // private WebElement errorPassword;
-    JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
-    WebElement emailInput = (WebElement) (javascriptExecutor.executeScript("return document.getElementById('email')"));
-    WebElement passwordInput = (WebElement) (javascriptExecutor.executeScript("return document.getElementById('password')"));
-    WebElement signInSubmitButton = (WebElement) (javascriptExecutor.executeScript("return document.querySelector('.ubsStyle')"));
-
     @FindBy(xpath = "//*[@id=\"email-err-msg\"]/app-error/div")
     private WebElement errorEmail;
-    @FindBy(css = ".header_navigation-menu-ubs ul a.pick-up-service")
-    private List<WebElement> aboutService;
-    @FindBy(css = "div.header_navigation-menu-ubs ul[role='tablist'] a[href='https://nowaste.com.ua/sort-station/']")
-    private List<WebElement> sortRules;
-    @FindBy(css = "div.header_navigation-menu-ubs ul[role='tablist'] a[href='https://shop.nowaste.com.ua/']")
-    private List<WebElement> ecoShop;
-    @FindBy(css = "div.header_navigation-menu-ubs ul[role='tablist'] a[href='#/greenCity']")
-    private List<WebElement> greenCity;
 
     private static WebDriver driver;
     private static boolean isSignInFormOpened = false;
+    JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
 
 
     @BeforeAll
@@ -94,8 +71,9 @@ public class TestSamples3 {
             "1, Про Сервіс",
     })
     public void testAboutService(int index, String expectedSubstring) {
+        driver.get("https://www.greencity.cx.ua/#/ubs");
+        List<WebElement> aboutService = (List<WebElement>) (javascriptExecutor.executeScript("return document.querySelector('div.header_navigation-menu-ubs ul a.pick-up-service')"));
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
         WebElement serviceLink = aboutService.get(index);
         wait.until(ExpectedConditions.elementToBeClickable(serviceLink)).click();
 
@@ -113,6 +91,13 @@ public class TestSamples3 {
             "greenCity, https://www.greencity.cx.ua/#/greenCity"
     })
     public void testNavigation(String linkType, String expectedUrl) {
+        List<WebElement> sortRules = (List<WebElement>) javascriptExecutor.executeScript(
+                "return document.querySelectorAll('div.header_navigation-menu-ubs ul[role=\"tablist\"] a[href=\"https://nowaste.com.ua/sort-station/\"]')");
+        List<WebElement> ecoShop = (List<WebElement>) javascriptExecutor.executeScript(
+                "return document.querySelectorAll('div.header_navigation-menu-ubs ul[role=\"tablist\"] a[href=\"https://shop.nowaste.com.ua/\"]')");
+        List<WebElement> greenCity = (List<WebElement>) javascriptExecutor.executeScript(
+                "return document.querySelectorAll('div.header_navigation-menu-ubs ul[role=\"tablist\"] a[href=\"#/greenCity\"]')");
+
         List<WebElement> links = null;
         switch (linkType) {
             case "sortRules":
@@ -135,11 +120,9 @@ public class TestSamples3 {
         }
     }
 
-
     private String getLink(WebElement element) {
         return element.getAttribute("href");
     }
-
 
     //My test
     @ParameterizedTest
@@ -161,10 +144,9 @@ public class TestSamples3 {
             isSignInFormOpened = true;
         }
 
-        //JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
-        //WebElement emailInput = (WebElement) (javascriptExecutor.executeScript("return document.getElementById('email')"));
-        //WebElement passwordInput = (WebElement) (javascriptExecutor.executeScript("return document.getElementById('password')"));
-        //WebElement signInSubmitButton = (WebElement) (javascriptExecutor.executeScript("return document.querySelector('.ubsStyle')"));
+        WebElement emailInput = (WebElement) (javascriptExecutor.executeScript("return document.getElementById('email')"));
+        WebElement passwordInput = (WebElement) (javascriptExecutor.executeScript("return document.getElementById('password')"));
+        WebElement signInSubmitButton = (WebElement) (javascriptExecutor.executeScript("return document.querySelector('.ubsStyle')"));
         switch (fieldName) {
             case "emailInput":
                 assertThat(emailInput.isDisplayed(), is(isDisplayed));
@@ -196,12 +178,12 @@ public class TestSamples3 {
             isSignInFormOpened = true;
             Thread.sleep(2000);
         }
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        WebElement welcomeText = (WebElement) (js.executeScript("return document.querySelector('div.container h1')"));
+
+        WebElement welcomeText = (WebElement) (javascriptExecutor.executeScript("return document.querySelector('div.container h1')"));
         assertThat(welcomeText.getText().trim(), is("З поверненням!"));
-        WebElement signInDetailsText = (WebElement) (js.executeScript("return document.querySelector('div.container h2')"));
+        WebElement signInDetailsText = (WebElement) (javascriptExecutor.executeScript("return document.querySelector('div.container h2')"));
         assertThat(signInDetailsText.getText(), is("Будь ласка, внесiть свої дані для входу."));
-        WebElement emailLabel = (WebElement) (js.executeScript("return document.querySelector(\"form.sign-in-form label[for='email']\")"));
+        WebElement emailLabel = (WebElement) (javascriptExecutor.executeScript("return document.querySelector(\"form.sign-in-form label[for='email']\")"));
         assertThat(emailLabel.getText(), is("Електронна пошта"));
 
         JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
@@ -229,7 +211,6 @@ public class TestSamples3 {
             "samplestesgreencity.com, Перевірте коректність введеної електронної адреси"
     })
     public void signInNotValid(String email, String message) {
-        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
         WebElement emailInput = (WebElement) (javascriptExecutor.executeScript("return document.getElementById('email')"));
         WebElement passwordInput = (WebElement) (javascriptExecutor.executeScript("return document.getElementById('password')"));
 
